70c3432f9a9b4cd2bb1b3bd29fd6b310995bc695
marcgrant21 <marcgrant10@gmail.com>
2021-03-31 14:16:49 -0500

    get the list of news

--- app/static/js/app.js
+++ app/static/js/app.js
@@ -52,16 +52,34 @@ app.component("app-footer", {
 });
 
 app.component("news-list", {
-  name: "newslist",
-  template: `
-  <div class="news">
- <h2>News</h2>
- <ul class="news__list">
- <li class="news__item">News item 1</li>
- <li class="news__item">News item 2</li>
- <li class="news__item">News item 3</li>
- </ul>
-</div>`,
+  name: "NewsList",
+  template: `<div class="news">
+  <h2>News</h2>
+  <ul class="news__list">
+  <li v-for="article in articles" 
+ class="news__item">{{ article.title }}</li>
+  </ul>
+ </div`,
+  created() {
+    let self = this;
+    fetch("https://newsapi.org/v2/top-headlines?country=us", {
+      headers: {
+        Authorization: "Bearer a54de67091a147099e10bd948e13b2f6",
+      },
+    })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        self.articles = data.articles;
+      });
+  },
+  data() {
+    return {
+      articles: [],
+    };
+  },
 });
 
 app.mount("#app");
